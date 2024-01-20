9278128c6dc4e954c2ed573d2afb26ac3a697f20
Shiva <shivaxwarrior@gmail.com>
2024-01-20 19:24:38 +0530

    removed safety layer completely

--- app/api/public/ai/route.ts
+++ app/api/public/ai/route.ts
@@ -18,6 +18,18 @@ export async function POST(request: NextRequest) {
             category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
             threshold: HarmBlockThreshold.BLOCK_NONE,
         },
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
     ];
 
     const reqBody = await request.json();
